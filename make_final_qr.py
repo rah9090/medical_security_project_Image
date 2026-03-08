579f4a3341e8571beab2b66193c59a8ae9ae75a7
RahmaFehan <rahmafehan1@gmail.com>
2026-03-09 01:42:47 +0300

    Update make_final_qr.py

--- make_final_qr.py
+++ make_final_qr.py
@@ -1,9 +1,9 @@
 import qrcode
 
-# الرابط الخاص بمستودعك الجديد
+
 my_github_url = "https://github.com/rah9090/Medical-Security--Imge"
 
-# إعدادات الـ QR ليكون واضحاً واحترافياً
+
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -13,11 +13,11 @@ qr = qrcode.QRCode(
 qr.add_data(my_github_url)
 qr.make(fit=True)
 
-# إنشاء الصورة وحفظها
+
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("official_project_qr.png")
 
 print("\n" + "="*40)
-print("✅ Done! Official QR Code generated.")
+print(" Done! Official QR Code generated.")
 print("Filename: official_project_qr.png")
 print("="*40)
