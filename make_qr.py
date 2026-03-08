f24f9a84c74460957a402590a200c2a8dadb4466
RahmaFehan <rahmafehan1@gmail.com>
2026-03-09 01:43:38 +0300

    make_qr.py

--- make_qr.py
+++ make_qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-# الرابط الذي تريد للناس فتحه (يمكنك تغييره لاحقاً لربط GitHub الخاص بك)
+
 link = "https://github.com/rhmhalmtyry/medical_security_project"
 
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
@@ -11,5 +11,5 @@ img = qr.make_image(fill_color="black", back_color="white")
 img.save("my_project_qr.png")
 
 print("\n---")
-print("✅ Success! Your QR Code is saved as: my_project_qr.png")
+print(" Success! Your QR Code is saved as: my_project_qr.png")
 print("---")
